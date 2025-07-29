6115b16ed995da29bd3af2d5dd98ea035568a9e7
yuri <yuri.durodie@gmail.com>
2025-07-29 16:33:59 +0200

    Updated for camera images with VIO

--- spectacularAI_vio_test/spectacularAI_vio_test/specAI_vio_images.py
+++ spectacularAI_vio_test/spectacularAI_vio_test/specAI_vio_images.py
@@ -37,7 +37,8 @@ class VIO(Node):
         self.left_img_pub = self.create_publisher(CompressedImage, 'left/image/compressed', 10)
         # self.right_img_pub = self.create_publisher(CompressedImage, 'right/image/compressed', 10)
         left_can = self.vio_pipeline.monoLeft
-        print(left_can)
+        print(left_can.getResolution())
+        # print(left_can)
         left_xout = self.pipeline.createXLinkOut()
         # xout1 = pipeline.create(dai.node.XLinkOut)
         left_xout.setStreamName("xoutleft")
