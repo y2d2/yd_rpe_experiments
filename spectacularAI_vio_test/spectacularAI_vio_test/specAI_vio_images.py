ce5bf558aa93d18347a4f63ee17c75cb3cd1865a
yuri <yuri.durodie@gmail.com>
2025-07-29 16:06:31 +0200

    Updated for camera images with VIO

--- spectacularAI_vio_test/spectacularAI_vio_test/specAI_vio_images.py
+++ spectacularAI_vio_test/spectacularAI_vio_test/specAI_vio_images.py
@@ -36,8 +36,8 @@ class VIO(Node):
         self.vio_pub = self.create_publisher(Odometry, 'vio', 10)
         self.left_img_pub = self.create_publisher(CompressedImage, 'left/image/compressed', 10)
         # self.right_img_pub = self.create_publisher(CompressedImage, 'right/image/compressed', 10)
-        left_xout = self.vio_pipeline.getLeftCameraOutput()
-        right_xout = self.vio_pipeline.getRightCameraOutput()
+        left_xout = self.vio_pipeline.xoutLeft
+        right_xout = self.vio_pipeline.xoutRight
         # Create device and start VIO session
         self.device = depthai.Device(self.pipeline)
         self.vio_session = self.vio_pipeline.startSession(self.device)
