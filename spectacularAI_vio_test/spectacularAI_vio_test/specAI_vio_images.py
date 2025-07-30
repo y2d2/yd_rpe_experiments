f456a5ba3e9cd43745dab46b4e1d3f37fa684d9b
Yoyo34181 <guillaumecrohin232@gmail.com>
2025-07-30 13:27:10 +0200

    try IMU

--- spectacularAI_vio_test/spectacularAI_vio_test/specAI_vio_images.py
+++ spectacularAI_vio_test/spectacularAI_vio_test/specAI_vio_images.py
@@ -145,11 +145,12 @@ class VIO(Node):
         self.imuQueue = self.device.getOutputQueue(name="xoutimu", maxSize=10, blocking=False)
         print(self.imuQueue, self.imuQueue.get())
         self.leftQueue = self.device.getOutputQueue(name="xoutleft", maxSize=4, blocking=False)
+        print(self.leftQueue, self.leftQueue.get())
         self.rightQueue = self.device.getOutputQueue(name="xoutright", maxSize=4, blocking=False)
         # self.rightQueue = self.device.getOutputQueue(name=self.rightEncoder.getOutputQueueName(), maxSize=4, blocking=False)
 
         # Start timer
-        self.timer = self.create_timer(0.001, self.processOutput)  # e.g., 100 Hz
+        self.timer = self.create_timer(0.01, self.processOutput)  # e.g., 100 Hz
 
 
     def processOutput(self):
